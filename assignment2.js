e1a6f5fd1a4159112cde1706be52e0dff1ca2511
suhana797 <65026739+suhana797@users.noreply.github.com>
2020-06-07 05:40:47 -0700

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,27 @@
+var name=" My self suhana nagdev";
+alert(name);
+var message ="Hello World";
+alert(message);
+var username=prompt("enter your name","your name is:");
+
+var age=prompt("enter your age","your age is:");
+
+var qualification=prompt("enter your qualification","your qualification is:");
+alert(username);
+alert(age);
+alert(qualification);
+
+var pizza ="                              pizza \n                       pizz \n                            piz\n                              pi\n                              p";
+alert(pizza);
+var example="My Emiail address is:";
+var email="suhana@gmail.com";
+var show=example+email;
+alert(show);
+
+var book="A smarter way to learn javascript";
+var A="I am trying to learn from book";
+var merge=A+book;
+alert(merge);
+document.getElementById("para");
+
+
---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Hello everyone! Welcome to my page");
+alert("Error!Please enter a valid password");
+alert("Welcome to JS Land...\nHAappy Coding!");
+alert("Welcome to JS Land...");
+alert("Happy Coding\nPrevent this page from creating additional dialogue");
+console.log("Hello...I can run JS through my web browser console");
